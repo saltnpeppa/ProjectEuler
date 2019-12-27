9cbd29b2f5890c1086bf1e7954f1678aa1a50d20
saltnpeppa <r03.sidhant@gmail.com>
2019-12-26 16:56:50 -0800

    problem 15

---
+++ src/15.kt
@@ -0,0 +1,26 @@
+
+fun generatePaths(n: Int): Long
+    {
+        var paths: ArrayList<Long> = arrayListOf()
+        var counter = 0
+        for (i in 2..n+1)
+        {
+            paths.add(i - 2, i.toLong())
+        }
+        println(paths)
+        while (counter < n - 1)
+        {
+            paths.set(0, paths.get(0).toLong() + 1L)
+            for (i in 1..n-1)
+            {
+                paths.set(i, paths.get(i - 1).toLong() + paths.get(i).toLong())
+            }
+            println(paths)
+            counter++
+        }
+        return paths.get(paths.size - 1)
+    }
+
+fun main() {
+    println(generatePaths(20))
+}
